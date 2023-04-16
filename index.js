79bc35f1ceadff2da84724a1584c49cf1666e809
Mateusz Tarchała <mateusztarch@gmail.com>
2023-04-16 21:07:04 +0200

    lab6

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const express = require('express');
-const config = require('./config').config;
-const questions = require('./questions').questions;
-const app = express();
-
-app.get('/api/questions', (request, response) => {
-    response.send(questions);
-});
-
-app.listen(config.port, function () {
-    console.info('Server is running at port ' + config.port);
-
-});
+require( 'babel-core/register' );
+require( './app/app');
